86d93ffed8d784855ce6f96b8d6221227b5752c1
ayush <ayushbhatt@204gmail.com>
2024-04-23 10:12:48 +0530

    initial

---
+++ app/(data)/ProjectData.js
@@ -0,0 +1,53 @@
+import img1 from "../assets/p1.png"
+import img2 from "../assets/p2.png"
+import img3 from "../assets/p3.png"
+export const projectData = [
+  {
+    id: 1,
+    title: "Snacks here",
+    description: "An online food ordering webapp where users can order their favorite food and get it delivered to their address.",
+    imageUrl: img1,
+    liveUrl: "https://example.com/snackshere",
+    sourceCode: "https://github.com/example/snackshere"
+  },
+  {
+    id: 2,
+    title: "File Sharing Webapp",
+    description: "A web application for securely sharing files with others. Users can upload files, generate shareable links, and manage their shared files.",
+    imageUrl: img2,
+    liveUrl: "https://example.com/makethingsfloat",
+    sourceCode: "https://github.com/example/makethingsfloat"
+  },
+  {
+    id: 3,
+    title: "Blog Writing Webapp",
+    description: "A web application for writing blogs. Users can create, edit, and delete their blogs. They can also share their blogs with others.",
+    imageUrl: img3,
+    liveUrl: "https://example.com/makethingsfloat",
+    sourceCode: "https://github.com/example/makethingsfloat"
+  },
+  {
+    id: 4,
+    title: "Make things float in air",
+    description: "Hover over this card to unleash the power of CSS perspective",
+    imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    liveUrl: "https://example.com/makethingsfloat",
+    sourceCode: "https://github.com/example/makethingsfloat"
+  },
+  {
+    id: 5,
+    title: "Make things float in air",
+    description: "Hover over this card to unleash the power of CSS perspective",
+    imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    liveUrl: "https://example.com/makethingsfloat",
+    sourceCode: "https://github.com/example/makethingsfloat"
+  },
+  {
+    id: 6,
+    title: "Make things float in air",
+    description: "Hover over this card to unleash the power of CSS perspective",
+    imageUrl: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    liveUrl: "https://example.com/makethingsfloat",
+    sourceCode: "https://github.com/example/makethingsfloat"
+  }
+];
